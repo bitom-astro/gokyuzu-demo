5d6316c15110e5efe4b29fc2ffa1dda6b299c20b
bitom-astro <kdrulc@gmail.com>
2026-05-25 21:01:46 +0300

    Create cassiopeia.ts

---
+++ app/cassiopeia.ts
@@ -0,0 +1,7 @@
+export const cassiopeia = [
+  { name: "Schedar", ra: 0.666, dec: 56.5 },
+  { name: "Caph", ra: 0.15, dec: 59.15 },
+  { name: "Tsih", ra: 0.95, dec: 60.7 },
+  { name: "Ruchbah", ra: 1.42, dec: 60.23 },
+  { name: "Segin", ra: 1.9, dec: 63.66 },
+];
